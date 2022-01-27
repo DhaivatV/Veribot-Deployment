1e38ee78dee224d4dfdf13b25bb325a4ff16a300
DhaivatV <71979720+DhaivatV@users.noreply.github.com>
2022-01-27 18:52:22 +0530

    Initialized

---
+++ myapp.py
@@ -0,0 +1,37 @@
+import uvicorn
+from fastapi import FastAPI
+from Incoming import Query
+import pandas as pd
+import numpy as np
+import spacy
+
+app = FastAPI()
+nlp = spacy.load("C:\\Users\\dvipa\\OneDrive\\Desktop\\Deployment\\en_my_pipeline")
+
+@app.get ('/')
+def index ():
+    return {'message': 'Hello, Stranger'}
+
+@app.post('/predict')
+def predict(data:Query):
+    data = data.dict()
+    print(data)
+    print("Hello")
+    text = data['text']
+    print(text)
+    texts = [text]
+    docs = [nlp.tokenizer(text) for text in texts]
+    textcat = nlp.get_pipe('textcat')
+    scores = textcat.predict(docs)
+    predicted_labels = scores.argmax(axis=1)
+    result = [textcat.labels[label] for label in predicted_labels]
+    print(result)
+    return {
+        'result': result[0]
+    }
+
+if __name__ == '__main__':
+    uvicorn.run(app, host= '127.0.0.1', port= 8000)
+
+
+
